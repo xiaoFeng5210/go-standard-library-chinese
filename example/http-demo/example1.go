7ae321f62888b95fd33879c379e4c825214a2b7c
Eadweard <aurora.coder.zqf@gmail.com>
2025-08-20 09:59:24 +0800

    fix

--- example/http-demo/example1.go
+++ example/http-demo/example1.go
@@ -2,7 +2,10 @@ package http_demo
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func Example1Server() {
@@ -15,7 +18,34 @@ func Example1Server() {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
+
+		file, fileHeader, err := r.FormFile("file")
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		saveFilePath := filepath.Join("assets", "uploads", "upload_test.jpg")
+		var saveFile *os.File
+		fmt.Printf("saveFilePath: %s\n", saveFilePath)
+		saveFile, err = os.Create(saveFilePath)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		_, err = io.Copy(saveFile, file)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("保存文件失败"))
+			return
+		}
+		saveFile.Close()
+		w.Write([]byte("文件上传成功"))
 	})
+
 	fmt.Println("Server is running on port 3333")
 	if err := http.ListenAndServe(":3333", nil); err != nil {
 		panic(err)
