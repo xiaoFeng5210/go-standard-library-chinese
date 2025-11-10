b2a6c137f62913fd40bcab652f86effa0b2b269a
Eadweard <aurora.coder.zqf@gmail.com>
2025-11-10 14:27:04 +0800

    fix

--- example/main.go
+++ example/main.go
@@ -1,39 +1,17 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type ListEgItem struct {
 	Name string
 	Desc string
 }
 
+type ItemEnum int
+
+const (
+	Item1 ItemEnum = iota
+	Item2
+	Item3
+)
+
 func main() {
-	// item := &ListEgItem{
-	// 	Name: "1",
-	// 	Desc: "描述1",
-	// }
-	// item2 := &ListEgItem{
-	// 	Name: "2",
-	// 	Desc: "描述2",
-	// }
-	// 创建测试数据
-	// list := []*ListEgItem{
-	// 	item,
-	// 	item2,
-	// }
-
-	listMap := map[string]interface{}{
-		"name": "1",
-		"desc": "描述1",
-	}
-
-	listMap2 := listMap
-
-	listMap2["name"] = "zqf"
-
-	jsonData, _ := json.Marshal(listMap)
-	fmt.Println(string(jsonData))
 }
